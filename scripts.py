d0d8a5c000151d2eece7ac137a537967222b78e8
Alexey Dyakonov <101695592+AlexeyDyakonov@users.noreply.github.com>
2023-06-12 21:41:11 +0500

    Update scripts.py
    Исправлены ошибки.
    Список похвал - перенесен в глобальные константы.
    Обновлено сообщение в исключениях.

--- scripts.py
+++ scripts.py
@@ -4,58 +4,54 @@ from datacenter.models import (Chastisement, Commendation, Lesson, Mark,
                                Schoolkid)
 
 
-def fix_marks(schoolkid_name):
+COMMENDATION = ['Молодец!',
+                'Отлично!',
+                'Хорошо!',
+                'Гораздо лучше, чем я ожидал!',
+                'Ты меня приятно удивил!',
+                'Ты, как всегда, точен!',
+                'Талантливо!',
+                'Потрясающе!',
+                'Замечательно!',
+                'Ты сегодня прыгнул выше головы!!',
+                'Здорово!',
+                'Я тобой горжусь!',
+                'Ты растешь над собой!']
+
+
+def check_schoolkid(schoolkid_name):
     try:
-        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-        bad_marks = Mark.objects.filter(schoolkid_name=child, points__lte=3).\
-            update(points=5)
-        bad_marks.save()
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        return schoolkid
     except Schoolkid.DoesNotExist:
         print("Такого имени нет")
     except Schoolkid.MultipleObjectsReturned:
-        print("Такого имени нет")
+        print("Найдено несколько совпадений")
+        
+
+def fix_marks(schoolkid_name):
+    schoolkid = check_schoolkid(schoolkid_name)
+    Mark.objects.filter(schoolkid=schoolkid, points__lte=3).update(points=5)
 
 
 def remove_chastisements(schoolkid_name):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-        remarks = Chastisement.objects.\
-            filter(schoolkid=schoolkid).delete()
-        remarks.save()
-    except Schoolkid.DoesNotExist:
-        print("Такого имени нет")
-    except Schoolkid.MultipleObjectsReturned:
-        print("Такого имени нет")
+    schoolkid = check_schoolkid(schoolkid_name)
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
 def create_commendation(schoolkid_name, lesson, year_of_study, group_letter):
-
     try:
+        schoolkid = check_schoolkid(schoolkid_name)
         lessons = Lesson.objects.filter(
             year_of_study=year_of_study,
             group_letter=group_letter,
             subject__title__contains=lesson).order_by('-subject').first()
-        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-        commendation_list = ['Молодец!',
-                             'Отлично!',
-                             'Хорошо!',
-                             'Гораздо лучше, чем я ожидал!',
-                             'Ты меня приятно удивил!',
-                             'Ты, как всегда, точен!',
-                             'Талантливо!',
-                             'Потрясающе!',
-                             'Замечательно!',
-                             'Ты сегодня прыгнул выше головы!!',
-                             'Здорово!',
-                             'Я тобой горжусь!',
-                             'Ты растешь над собой!']
-        commendation_text = random.choice(commendation_list)
+        commendation_text = random.choice(COMMENDATION)
         Commendation.objects.create(text=commendation_text,
                                     created=lessons.date,
-                                    schoolkid=child,
+                                    schoolkid=schoolkid,
                                     subject=lessons.subject,
                                     teacher=lessons.teacher)
-    except Schoolkid.DoesNotExist:
-        print("Такого имени нет")
-    except Schoolkid.MultipleObjectsReturned:
-        print("Такого имени нет")
+    except AttributeError:
+        print("Неверно введен предмет или год обучения или литер класса")
+        
